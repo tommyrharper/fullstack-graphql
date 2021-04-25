f29a21da5b482cc1ffe6b0a3f6d903927f3292a7
tommyrharper <thomasrobertharper@gmail.com>
2021-04-25 13:08:14 +0100

    Add schema types

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,16 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
-
+  type User {
+    id: ID!
+    username: String!
+  }
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
 `;
 
 module.exports = typeDefs
