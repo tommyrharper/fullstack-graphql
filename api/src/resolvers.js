3402d358d7e9f6d0138df70b5231561b9ba96806
tommyrharper <thomasrobertharper@gmail.com>
2021-04-30 21:58:36 +0100

    Update resolvers interfaces

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,20 +5,27 @@
 
 module.exports = {
   Query: {
-    pets(_, __, ctx) {
-      return ctx.models.Pet.findMany()
+    pets(_, {input}, ctx) {
+      // return ctx.models.Pet.findMany({ id: input.id })
+      return ctx.models.Pet.findMany(input)
+      // return [{ id: 1, name: 'moose' }, { id: 2, name: 'garfield' }]
+    },
+    pet(_, {input}, ctx) {
+      return ctx.models.Pet.findOne(input)
     }
   },
-  // Mutation: {
-    
-  // },
-  // Pet: {
-  //   img(pet) {
-  //     return pet.type === 'DOG'
-  //       ? 'https://placedog.net/300/300'
-  //       : 'http://placekitten.com/300/300'
-  //   }
-  // },
+  Mutation: {
+    newPet(_, {input}, ctx) {
+      return ctx.models.Pet.create(input)
+    }
+  },
+  Pet: {
+    // img(pet) {
+    //   return pet.type === 'DOG'
+    //     ? 'https://placedog.net/300/300'
+    //     : 'http://placekitten.com/300/300'
+    // }
+  },
   // User: {
     
   // }
