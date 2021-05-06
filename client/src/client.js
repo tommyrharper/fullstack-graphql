89c240f98ebbf77a10424bbcef35e02c35d47004
tommyrharper <thomasrobertharper@gmail.com>
2021-05-06 21:01:01 +0100

    Add delay middleware to client.js

--- client/src/client.js
+++ client/src/client.js
@@ -1,6 +1,8 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { ApolloLink } from 'apollo-link';
 import gql from 'graphql-tag'
 
 /**
@@ -8,7 +10,19 @@ import gql from 'graphql-tag'
  */
 
 // const link = new HttpLink({ uri: 'http://rickandmortyapi.com/graphql' })
-const link = new HttpLink({ uri: 'http://localhost:4000' })
+const http = new HttpLink({ uri: 'http://localhost:4000' })
+const delay = setContext(
+  request =>
+    new Promise((success, fail) => {
+      setTimeout(() => {
+        success()
+      }, 800)
+    })
+)
+const link = ApolloLink.from([
+  delay,
+  http
+])
 const cache = new InMemoryCache()
 
 const client = new ApolloClient({
