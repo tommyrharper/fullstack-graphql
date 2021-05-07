ed4c0f60569f14bca3c37836290b91a653ad8254
tommyrharper <thomasrobertharper@gmail.com>
2021-05-07 23:58:59 +0100

    Create petsAtom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,10 @@
 import { Switch, Route } from 'react-router-dom'
 import React, {Fragment} from 'react'
+import { atom, Provider, useAtom } from "jotai";
 import Header from './Header'
 import Pets from '../pages/Pets'
+
+export const petsAtom = atom([])
 
 const App = () => (
   <Fragment>
